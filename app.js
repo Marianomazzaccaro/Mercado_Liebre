5e9595a97bce1d33de43f559b8f02400dd95b679
mmazzaccaro <m.mazzaccaro@hotmail.com>
2021-01-25 20:29:30 -0300

    MercadoLiebre

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path= require("path")
+
+const app = express()
+
+const publicPath = path.resolve(__dirname,"./public")
+
+
+app.use(express.static(publicPath) ); //Queremos tener la carpeta public como un recurso de archivos estaticos para siempre poder consumirla.
+
+app.listen(3000, () => { console.log( "Servidor corriendo en el puerto 3000");
+
+
+app.get('/home',(req,res) => {
+    res.sendFile(path.resolve('./views/home.html'));
+});
+
+
+
+
+
+
+})
