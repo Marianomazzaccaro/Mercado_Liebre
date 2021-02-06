945000ebfea783d8b04361f6848d6d6f6b64fbf7
mmazzaccaro <m.mazzaccaro@hotmail.com>
2021-02-05 21:08:25 -0300

    V final MLiebre Mmazzaccaro

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express()
 const publicPath = path.resolve("public")
 
 
-app.use(express.static(publicPath) ); //Queremos tener la carpeta public como un recurso de archivos estaticos para siempre poder consumirla.
+app.use(express.static("public") ); //Queremos tener la carpeta public como un recurso de archivos estaticos para siempre poder consumirla.
 
 /*app.listen(3000, () => { console.log( "Servidor corriendo en el puerto 3000");})*/
 app.listen(process.env.PORT || 3000, function(){
